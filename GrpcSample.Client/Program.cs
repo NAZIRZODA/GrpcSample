54d3f0eeae2bc93f881593d669a8d7e1e25cd0f2
Dilshod <dilshod@snoonu.com>
2024-11-23 17:59:15 +0500

    gRPC demo with customer serializer

---
+++ GrpcSample.Client/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Threading.Tasks;
+using Grpc.Core;
+using Grpc.Core.Utils;
+using GrpcSample.Shared;
+
+namespace GrpcSample.Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RunAsync().Wait();
+        }
+
+        private static async Task RunAsync()
+        {
+            var channel = new Channel("127.0.0.1", 5000, ChannelCredentials.Insecure);
+            var invoker = new DefaultCallInvoker(channel);
+            using (var call = invoker.AsyncDuplexStreamingCall(Descriptors.Method, null, new CallOptions { }))
+            {
+                var responseCompleted = call.ResponseStream
+                    .ForEachAsync(async response => { Console.WriteLine($"Got response: {response.Payload} {response.NullableCollection}, {response.NullablePayload}"); });
+
+                for (int i = 0; i < 100; i++)
+                {
+                    await call.RequestStream.WriteAsync(new CustomRequest
+                        { 
+                            Payload = i,
+                            NullablePayload = null,
+                            NullableCollection = [1, 2, 3],
+                            Text = "Test string"
+                        });
+                }
+
+                await call.RequestStream.CompleteAsync();
+                await responseCompleted;
+            }
+            
+            Console.WriteLine("Press enter to stop...");
+            Console.ReadLine();
+
+            await channel.ShutdownAsync();
+        }
+    }
+}
---
+++ GrpcSample.Shared/Messages.cs
@@ -0,0 +1,37 @@
+﻿#nullable enable
+
+using System.Collections.Generic;
+using Bond;
+
+namespace GrpcSample.Shared
+{
+    [Schema]
+    public class CustomRequest
+    {
+        [Id(0)]
+        public int Payload { get; set; }
+        
+        [Id(1)]
+        public int? NullablePayload { get; set; }
+
+        [Id(2)]
+        public List<int>? NullableCollection { get; set; }
+
+        [Id(3)] public string Text { get; set; } = string.Empty;
+    }
+
+    [Schema]
+    public class CustomResponse
+    {
+        [Id(0)]
+        public int Payload { get; set; }
+        
+        [Id(1)]
+        public int? NullablePayload { get; set; }
+
+        [Id(2)]
+        public List<int>? NullableCollection { get; set; }
+
+        [Id(3)] public string Text { get; set; } = string.Empty;
+    }
+}
